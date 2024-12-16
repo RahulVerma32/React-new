33cb516cb11b2d41076c921a21402a33fda2a6b6
Rahul Verma <vermarahul7344@gmail.com>
2024-12-16 18:22:53 +0530

    custom react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,10 @@
 import Youtube from "./Youtube"
 
 function App() {
-
+    const username='rahul'
   return (
-    <>  <h1>Vite React App</h1>
-        <h1>Vite React App</h1>
+    <>  <h1>Vite React App {9*8+35-99*7}{" "} 
+  </h1> 
     <Youtube/>
     </>
 
