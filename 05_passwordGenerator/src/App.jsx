504a52d9b5451437a8bea4dbc48b177687cf5af9
Rahul Verma <vermarahul7344@gmail.com>
2025-04-12 18:50:30 +0530

    router

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+ const [length,setLength]=useState(8)
   return (
     <>
     <h1 className='text-4xl text-center'>Password Generator</h1>
